12c83cd5320f4402d09ed1800239667807ae4cce
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2026-01-14 13:48:03 +0200

    Add more checks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using System.Diagnostics;
 
-Process.Start("java", [ "-jar", Path.Combine(Directory.GetCurrentDirectory(), "ATLauncher.jar") ]);
+var javaHome = Environment.GetEnvironmentVariable("JAVA_HOME"); // Adjust this if Java is not in your system PATH
+var javaPath = javaHome != null ? Path.Combine(javaHome, "bin", "java") : "java";
+
+Console.WriteLine($"Using Java path: {javaPath}");
+
+var process = Process.Start(javaPath, [ "-jar", Path.Combine(Directory.GetCurrentDirectory(), "ATLauncher.jar") ]);
+
+if (process == null)
+{
+    Console.WriteLine("Failed to start ATLauncher process.");
+    return;
+}
+
+await process.WaitForExitAsync();
+
+Console.ReadKey();
